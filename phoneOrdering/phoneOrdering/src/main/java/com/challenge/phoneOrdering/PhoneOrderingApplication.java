80eb8878d2bcb16fce98debe3475f04bdc8a4715
miguel.montero <miguel.montero@niit-tech.com>
2018-11-29 23:28:32 +0100

    Carga inicial del esqueleto del proyecto del microservicio phoneOrdering

---
+++ phoneOrdering/phoneOrdering/src/main/java/com/challenge/phoneOrdering/PhoneOrderingApplication.java
@@ -0,0 +1,12 @@
+package com.challenge.phoneOrdering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneOrderingApplication.class, args);
+	}
+}
